90692f79dd3b96e0381ee4e9a98e3319c631536b
cmolinan <cmolinan@gmail.com>
2023-06-22 12:17:15 -0500

    Make MiPrimerToken upgradeable and minting 1_000_000 tokens

--- contracts/MiPrimerToken.sol
+++ contracts/MiPrimerToken.sol
@@ -1,13 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract MyTokenMiPrimerToken is ERC20, Ownable {
-    constructor() ERC20("MyTokenMiPrimerToken", "MPRTKN") {}
+contract MiPrimerToken is
+    Initializable,
+    ERC20Upgradeable,
+    UUPSUpgradeable,
+    OwnableUpgradeable
+{
+    function initialize(string memory _name, string memory _symbol) public initializer {
+        __ERC20_init(_name, _symbol);
+        _mint(msg.sender, 10 ** 6 * 10 ** decimals());
+    }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+   
 }
