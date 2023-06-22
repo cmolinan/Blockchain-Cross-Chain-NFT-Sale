dac42076ef8632c40e84ff3aeea861f716bb7b2d
cmolinan <cmolinan@gmail.com>
2023-06-22 18:08:49 -0500

    Upgrading libraries and including localhost network to Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,14 @@ require("@openzeppelin/hardhat-upgrades");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  networks: {
+  defaultNetwork: "localhost",
+  networks: {    
+    localhost: {
+      url: "HTTP://127.0.0.1:8545",
+      timeout: 800000,
+      gas: "auto",
+      gasPrice: "auto",
+    },    
     mumbai: {
       url: process.env.MUMBAI_TESNET_URL,
       accounts: [process.env.PRIVATE_KEY || ""],
