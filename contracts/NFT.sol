4ce754074cf492c1ff38db71df5592e05d0592f7
cmolinan <cmolinan@gmail.com>
2023-06-23 04:28:38 -0500

    First version of NFT Smart Contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,41 +6,39 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract MiPrimerNft {
+contract MiPrimerNft is ERC721, Pausable, AccessControl, ERC721Burnable {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() /**ERC721("MiPrimerNft", "MPRNFT") */ {
+    constructor() ERC721("MiPrimerNft", "MPRNFT") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://QmXcuyR7xGKWzpvyTzKXo9rUkV94PDa7DFRU9jWPYdrWVx/";
-        
+        return "ipfs://QmXcuyR7xGKWzpvyTzKXo9rUkV94PDa7DFRU9jWPYdrWVx/";        
     }
 
-    function pause() public {
+    function pause() public onlyRole(PAUSER_ROLE) whenNotPaused  {
         _pause();
     }
 
-    function unpause() public {
+    function unpause() public onlyRole(PAUSER_ROLE) whenPaused {
         _unpause();
     }
-
-    function safeMint(address to, uint256 id) public {
-        // Se hacen dos validaciones
-        // 1 - Dicho id no haya sido acuñado antes
-        // 2 - Id se encuentre en el rando inclusivo de 1 a 30
-        //      * Mensaje de error: "Public Sale: id must be between 1 and 30"
+    
+    function safeMint(address _to, uint256 _tokenId) public {
+        require(!_exists(_tokenId), "It was already minted");
+        require( _tokenId >= 1 && _tokenId <= 30, "Public Sale: tokenId must be between 1 and 30");
+        _safeMint(_to, _tokenId);
     }
 
     // The following functions are overrides required by Solidity.
 
-    // function supportsInterface(
-    //     bytes4 interfaceId
-    // ) public view override(ERC721, AccessControl) returns (bool) {
-    //     return super.supportsInterface(interfaceId);
-    // }
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 }
