edad5d757f644a14acc839996c492ca2f760d4dc
cmolinan <cmolinan@gmail.com>
2023-06-23 13:33:48 -0500

    Coding method deployMumbai in script deploy.js for the deployment of NFT SC

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -27,28 +27,36 @@ async function deployTokens() {
     
 }
 
-deployTokens()
+async function deployMumbai() {
+  var relayerAddress = "0x9ba986566f59441E2F7d7A30Eb2A935ccEE58fc7";
+  var name = "MiPrimerNft";
+  var symbol = "MPRNFT";
+
+  var nftContract = await deploySC("MiPrimerNft", [name, symbol]);
+  var implementation = await printAddress("NFT", nftContract.address);
+
+// set up
+  await ex(nftContract, "grantRole", [MINTER_ROLE, relayerAddress], "GR");
+
+  await verify(implementation, "MiPrimerNft", []);  
+}
+
+
+var MINTER_ROLE = getRole("MINTER_ROLE");
+
+deployTokens()   //have to run in Goerli Chain !!
 .then(() => process.exit(0))
 .catch((error) => {
   console.error(error);
   process.exitCode = 1; // exitcode quiere decir fallor por error, terminacion fatal
 });
 
-// var MINTER_ROLE = getRole("MINTER_ROLE");
-// var BURNER_ROLE = getRole("BURNER_ROLE");
-
-// async function deployMumbai() {
-//   var relayerAddress = "0xeb0868cf925105ac466c2b19039301e904061514";
-//   var name = "Mi Primer NFT";
-//   var symbol = "MPRNFT";
-//   var nftContract = await deploySC("MiPrimerNft", [name, symbol]);
-//   var implementation = await printAddress("NFT", nftContract.address);
-
-//   // set up
-//   await ex(nftTknContract, "grantRole", [MINTER_ROLE, relayerAddress], "GR");
-
-//   await verify(implementation, "MiPrimerNft", []);
-// }
+deployMumbai()  //have to run in Mumbai Chain !!
+.then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exitCode = 1; // exitcode quiere decir fallor por error, terminacion fatal
+});
 
 
 // async function deployGoerli() {
@@ -57,16 +65,3 @@ deployTokens()
 //   // Extraer el address del gnosis safe y pasarlo al contrato con un setter
 //   var gnosis = { address: "" };
 // }
-
-
-// // deployMumbai()
-// // deployGoerli()
-// // deployMiPrimerToken();
-// deployUSDCoin();
-// // deployMiPrimerNft();
-
-//   //
-//   .catch((error) => {
-//     console.error(error);
-//     process.exitCode = 1;
-//   });
