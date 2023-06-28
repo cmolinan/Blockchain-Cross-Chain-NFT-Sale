0dc2a2866cdf236d8ca8b4a3ed17704795cd3d24
cmolinan <cmolinan@gmail.com>
2023-06-27 19:01:22 -0500

    Changes in NFT Contract address inside OZD autotask script

--- defender/Defender-autotask.js
+++ defender/Defender-autotask.js
@@ -28,10 +28,11 @@ exports.handler = async function (data) {
   console.log("CMN-params",winnerAccount,nftId, "-FIN");
   
   // Ejecutar 'SafeMint' en Mumbai del contrato MiPrimerNFT
-  var miPrimerNFT = "0x9e03a5f5447af8A0Df23F663a86EcDf62aC1B571";
+  // var miPrimerNFT = "0x9e03a5f5447af8A0Df23F663a86EcDf62aC1B571"; // antiguo
+  var miPrimerNFT = "0xe97de0F932de655C7edd7685324415B541DAE71e";
+  
   var tokenAbi = ["function safeMint(address _to, uint256 _tokenId)"];
   var tokenContract = new ethers.Contract(miPrimerNFT, tokenAbi, signer);
-  // var tx = await tokenContract.safeMint("0x7A30a1401a37FBAFbb7db0207a1658511096B861", 8);
   var tx = await tokenContract.safeMint(winnerAccount, nftId);
   var res = await tx.wait();
   return res;
